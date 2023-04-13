e6e31618446caac46b17b035274e8b8d895e56c5
Razvan Gorea <razvan.gorea2@mail.dcu.ie>
2023-04-13 19:46:51 +0100

    Worked further on create a student page

--- src/pages/NewStudent.js
+++ src/pages/NewStudent.js
@@ -8,30 +8,49 @@ export function CreateStudent() {
       .then(response => response.json())
       .then(data => {
         setData(data);
-        console.log(data);
       });
   }, []);
+  
+  const handleSubmit = (event) => {
+    if (event){
+        event.preventDefault();
+        const student_id = event.target.student_id;
+        const first_name = event.target.first_name;
+        const last_name = event.target.last_name;
+        const email = event.target.email;
+        const cohort = event.target.cohort;
+    
+        const student = {
+            student_id: String(student_id.value),
+            first_name: String(first_name.value),
+            last_name: String(last_name.value),
+            email: String(email.value),
+            cohort: 'http://127.0.0.1:8000/api/student/' + String(cohort[cohort.selectedIndex].value),
+    }
 
+    console.log(student);
+}
+  };
   return (
     <div>
-        <form>
+        <form onSubmit={(event) => handleSubmit(event) }>
             <div>
                 <h3>Enter details for a new student:</h3>
             </div>
             <div>
-                <input type="text" name="student_id" placeholder="Student's ID" required/>
+                <input type="text" id="student_id" name="student_id" placeholder="Student's ID" required/>
             </div>
             <div>
                 <br></br>
-                <input type="text" name="first_name" placeholder="Student's First Name" required/>
+                <input type="text" id="first_name" name="first_name" placeholder="Student's First Name" required/>
             </div>
             <div>
                 <br></br>
-                <input type="text" name="last_name" placeholder="Student's Last Name" required/>
+                <input type="text" id="last_name" name="last_name" placeholder="Student's Last Name" required/>
             </div>
             <div>
                 <br></br>
-                <input type="text" name="email" placeholder="Student's Email" required/>
+                <input type="text" id="email" name="email" placeholder="Student's Email" required/>
             </div>
             <div>
                 <p>Choose a cohort for the new student:</p>
@@ -40,7 +59,7 @@ export function CreateStudent() {
                 {data !== null ?
                                 <select id="cohort" name="cohort" required>
                                     {data.map((cohort) => (
-                                        <option key={cohort.id} value={cohort.degree} >{cohort.name}</option>
+                                        <option key={cohort.id} value={cohort.id} >{cohort.name}</option>
                                     ))} 
                                 </select>
                         : null}
